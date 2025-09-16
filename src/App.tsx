e3d3a58e48d104a485c3ab3c2185e5ea2d53f736
Surace6063 <sureshthapa6063@gmail.com>
2025-09-16 19:52:15 +0545

    add vercel file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { Route, Routes } from "react-router-dom";
-import NavBar from  "./components/NavBar"
-import HomePage from "./pages/HomePage";
-import CodeLittle from "./pages/CodeLittle";
-import CodeJunior from "./pages/CodeJunior";
-import CodeMater from "./pages/CodeMater";
+import { Suspense, lazy } from "react";
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+
+// Lazy load pages
+const HomePage = lazy(() => import("./pages/HomePage"));
+const CodeLittle = lazy(() => import("./pages/CodeLittle"));
+const CodeJunior = lazy(() => import("./pages/CodeJunior"));
+const CodeMater = lazy(() => import("./pages/CodeMater"));
 
 export default function App() {
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/little-coder" element={<CodeLittle />} />
-        <Route path="/code-junior" element={<CodeJunior />} />
-        <Route path="/code-master" element={<CodeMater />} />
-      </Routes>
+      <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/little-coder" element={<CodeLittle />} />
+          <Route path="/code-junior" element={<CodeJunior />} />
+          <Route path="/code-master" element={<CodeMater />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
